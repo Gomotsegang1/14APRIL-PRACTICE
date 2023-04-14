2a4bb8380726be7da94ff55f596dcb299bdb59d6
Gomotsegang1 <nadinematloa@gmail.com>
2023-04-14 10:54:26 +0200

    first commit

---
+++ 14April.ts
@@ -0,0 +1,8 @@
+//Function to determine if a function is even/odd
+let value1: number= 5;
+if (value1%2==0){
+    console.log("Value 1 is an even number")
+}
+else{
+console.log("Value 1 is an odd number")
+}
